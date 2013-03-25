9f6edb9e1550144fb1646d28c14e0b5a50525246
bevyn <bevijn@gmail.com>
2013-03-25 18:28:31 +0100

    first commit, working version of UU_TC's ParseLib.Simple
    Signed-off-by: Pepijn Kokke <wen.kokke@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 ParseLib
 ========
 
-A port of Utrecht University's ParseLib (from Haskell) to Frege, to allow usage of parser combinators with Java.
+ParseLib is a port of Utrecht University's Parser Combinators (from Haskell) to Frege,
+to allow the usage of parser combinator parsers in a JVM environment.
+
+The current version is a port of the `uu-tc` package, which can be found [on hackage][uu-tc];
+more specifically, it ports the default library (known as `ParseLib.Simple`).
+This means that the library overwrites a number of default functions, mostly from the
+`Applicative` class[^overrides].
+
+[uu-tc]: http://hackage.haskell.org/package/uu-tc
+[^overrides]: The specific list is `>>=`, `<$>`, `<*>`, `<*`, `*>` and `sequence`.
---
+++ src/java/parselib/examples/JArith.java
@@ -0,0 +1,40 @@
+package parselib.examples;
+
+import parselib.examples.Arith.TExpr;
+import frege.prelude.PreludeBase.TMaybe;
+import frege.prelude.PreludeBase.TMaybe.DJust;
+import frege.runtime.Lazy;
+
+public final class JArith {
+
+	
+	/**
+	 * Output looks as follows.
+	 * <pre>
+	 * Add (Num 1) (Num 3)
+	 * Mul (Add (Num 10) (Num 4)) (Mul (Num 3) (Num 8))
+	 * no parse
+	 * no parse
+	 * </pre?
+	 */
+	public static void main(String[] args) {
+		show(parse("(+ 1 3)"));
+		show(parse("(* (+ 10 4) (* 3 8))"));
+		show(parse("(+)"));
+		show(parse("(+ a)"));
+	}
+	
+	public static final TExpr parse(String input) {
+		return fromJust(Arith.parse(input));
+	}
+	
+	public static final void show(TExpr expr) {
+		System.out.println((expr == null) ? "no parse" : Arith.IShow_Expr.show(expr));
+	}
+	
+	public static final <R> R fromJust(TMaybe maybe) {
+		final DJust dJust = maybe.<TMaybe> forced()._Just();
+		return (dJust == null) ? null : ((Lazy) dJust.mem1).<R> forced();
+	}
+
+}
